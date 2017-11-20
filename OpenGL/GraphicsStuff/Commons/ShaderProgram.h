04e2c098ea2f28dedbf72a0f8d21ee90209c41df
botarga <mariovivasdavid@hotmail.es>
2017-11-20 19:46:47 +0100

    Shader program class added

--- OpenGL/GraphicsStuff/Commons/ShaderProgram.h
+++ OpenGL/GraphicsStuff/Commons/ShaderProgram.h
@@ -7,28 +7,88 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <sstream>
+#include <fstream>
 
 class ShaderProgram
 {
+private:
+	GLuint mId;
+
 public:
 
 	ShaderProgram(std::vector<std::pair<const char*, int>> shaders)
 	{
+		mId = glCreateProgram();
 
+		std::vector<GLuint> shaderIds(shaders.size());
+		int cont = 0;
+		for (const auto& s : shaders) {
+			std::ifstream shaderFile(s.first);
+			std::string codeStr((std::istreambuf_iterator<char>(shaderFile)),
+				(std::istreambuf_iterator<char>()));
+			shaderIds[cont] = CompileShader(codeStr.c_str(), s.second);
+			glAttachShader(mId, shaderIds[cont++]);
+		}
+
+		glLinkProgram(mId);
+		GLint success;
+		glGetProgramiv(mId, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramiv(mId, GL_INFO_LOG_LENGTH, &success);
+			char* message = new char[success];
+			glGetProgramInfoLog(mId, success, 0, message);
+			std::cerr << "PROGRAM_LINK_ERROr:" << message << std::endl;
+			delete[] message;
+		}
+
+		for (const auto& id : shaderIds) {
+			glDetachShader(mId, id);
+			glDeleteShader(id);
+		}
+	}
+
+	GLuint CompileShader(const char* code, int type) {
+		GLuint shader = glCreateShader(type);
+		glShaderSource(shader, 1, &code, 0);
+		glCompileShader(shader);
+		GLint success;
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			switch (type) {
+			case GL_VERTEX_SHADER:
+				std::cerr << "VERTEX_SHADER_ERROR::";
+				break;
+			case GL_FRAGMENT_SHADER:
+				std::cerr << "FRAGMENT_SHADER_ERROR::";
+				break;
+			case GL_GEOMETRY_SHADER:
+				std::cerr << "GEOMETRY_SHADER_ERROR::";
+				break;
+
+			}
+			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &success);
+			char* message = new char[success];
+			glGetShaderInfoLog(shader, success, 0, message);
+			std::cerr << "COMPILE_ERROR:" << message << std::endl;
+			delete[] message;
+		}
+
+		return shader;
 	}
 
 	void Use()
 	{
-
+		glUseProgram(mId);
 	}
 
 	void SetInt(const char* name, int value)
 	{
-
+		glUniform1i(glGetUniformLocation(mId, name), value);
 	}
 
 	void SetMat4(const char* name, const glm::mat4& value)
 	{
-
+		glUniformMatrix4fv(glGetUniformLocation(mId, name), 1, false, glm::value_ptr(value));
 	}
 };
