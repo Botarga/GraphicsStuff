0043798505e6d2fd6a0e841d78f87310814fc622
botarga <mariovivasdavid@hotmail.es>
2017-11-30 00:34:07 +0100

    Specular and diffuse ended

--- OpenGL/OpenTK/FormGrower/LoadTexture/Program.cs
+++ OpenGL/OpenTK/FormGrower/LoadTexture/Program.cs
@@ -8,11 +8,11 @@ using GraphicUtils;
 using System.Drawing;
 using System.Drawing.Imaging;
 
-namespace SpecularAndDiffuseMapping
+namespace LoadTexture
 {
     class MyGame : GameWindow
     {
-        float[] vertices = new float[]{
+        /*float[] vertices = new float[]{
             // positions          // normals           // texture coords
             -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
              0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
@@ -56,19 +56,23 @@ namespace SpecularAndDiffuseMapping
             -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
             -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
         };
+        */
+        float[] vertices = new float[]{
+            -0.5f, 0.5f, 0.0f,      0.0f, 1.0f,
+            0.5f, 0.5f, 0.0f,       1.0f, 1.0f,
+            -0.5f, -0.5f, 0.0f,     0.0f, 0.0f,
+
+             0.5f, 0.5f, 0.0f,      1.0f, 1.0f,
+            -0.5f, -0.5f, 0.0f,     0.0f, 0.0f,
+            0.5f, -0.5f, 0.0f,      1.0f, 0.0f
+        };
 
         int VAO, VBO;
         double deltaTime;
-        Vector3 cubeColor = new Vector3(1.0f, 0.0f, 0.0f);
-        Vector3 lightColor = new Vector3(1.0f, 1.0f, 1.0f);
-        Vector3 lightPosition = new Vector3(0.0f, 0.0f, 2.0f);
-
-        Random gen = new Random();
+        
         Camera cam = new Camera();
         int program;
-        int lightShader;
         int cubeTexture;
-        int cubeSpecularTexture;
 
         public MyGame(int width, int height) : base(width, height, new OpenTK.Graphics.GraphicsMode(32, 24, 0, 4))
         {
@@ -87,50 +91,27 @@ namespace SpecularAndDiffuseMapping
                 cam.ResetCursor(this);
             }
 
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-            Matrix4 model, view, projection;
+            GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
 
             GL.ActiveTexture(TextureUnit.Texture0);
             GL.BindTexture(TextureTarget.Texture2D, cubeTexture);
-            GL.ActiveTexture(TextureUnit.Texture1);
-            GL.BindTexture(TextureTarget.Texture2D, cubeSpecularTexture);
+            GL.UseProgram(program);
+
+            Matrix4 model, view, projection;
+            model = Matrix4.Identity;
+            view = cam.GetViewMatrix();
+            projection = cam.GetProjectionMatrix();
+
+            GL.UniformMatrix4(GL.GetUniformLocation(program, "model"), false, ref model);
+            GL.UniformMatrix4(GL.GetUniformLocation(program, "view"), false, ref view);
+            GL.UniformMatrix4(GL.GetUniformLocation(program, "projection"), false, ref projection);
+
 
             GL.BindVertexArray(VAO);
-            GL.UseProgram(program);
-            {
-                model = Matrix4.Identity;
-                view = cam.GetViewMatrix();
-                projection = cam.GetProjectionMatrix();
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
 
-                // Matrix dat
-                GL.UniformMatrix4(GL.GetUniformLocation(program, "model"), false, ref model);
-                GL.UniformMatrix4(GL.GetUniformLocation(program, "view"), false, ref view);
-                GL.UniformMatrix4(GL.GetUniformLocation(program, "projection"), false, ref projection);
-                GL.Uniform3(GL.GetUniformLocation(program, "viewPos"), cam.Position);
-
-                GL.Uniform3(GL.GetUniformLocation(program, "light.position"), lightPosition);
-                GL.Uniform3(GL.GetUniformLocation(program, "light.ambient"), new Vector3(0.2f));
-                GL.Uniform3(GL.GetUniformLocation(program, "light.diffuse"), new Vector3(0.5f));
-                GL.Uniform3(GL.GetUniformLocation(program, "light.specular"), new Vector3(1.0f));
-
-                GL.Uniform3(GL.GetUniformLocation(program, "material.specular"), new Vector3(0.5f));
-                GL.Uniform1(GL.GetUniformLocation(program, "material.shininess"), 32.0f);
-
-                GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
-
-            }
-            GL.UseProgram(lightShader);
-            {
-                model = Matrix4.CreateScale(0.2f) * Matrix4.CreateTranslation(lightPosition);
-                GL.UniformMatrix4(GL.GetUniformLocation(lightShader, "model"), false, ref model);
-                GL.UniformMatrix4(GL.GetUniformLocation(lightShader, "view"), false, ref view);
-                GL.UniformMatrix4(GL.GetUniformLocation(lightShader, "projection"), false, ref projection);
-
-
-                GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
-            }
             SwapBuffers();
+
         }
 
         public int LoadTexture(string file)
@@ -168,18 +149,6 @@ namespace SpecularAndDiffuseMapping
             if (state.IsKeyDown(Key.Escape))
                 Environment.Exit(0);
 
-            if (state.IsKeyDown(Key.I))
-                lightPosition.Z -= 0.05f;
-            if (state.IsKeyDown(Key.K))
-                lightPosition.Z += 0.05f;
-            if (state.IsKeyDown(Key.J))
-                lightPosition.X -= 0.05f;
-            if (state.IsKeyDown(Key.L))
-                lightPosition.X += 0.05f;
-            if (state.IsKeyDown(Key.U))
-                lightPosition.Y += 0.05f;
-            if (state.IsKeyDown(Key.O))
-                lightPosition.Y -= 0.05f;
 
         }
 
@@ -194,48 +163,32 @@ namespace SpecularAndDiffuseMapping
         {
             base.OnLoad(e);
 
-            GL.ClearColor(0.01f, 0.01f, 0.01f, 1.0f);
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
             GL.Enable(EnableCap.DepthTest);
             CursorVisible = false;
 
-            program = LoadProgram(new List<Tuple<string, ShaderType>> {
-                new Tuple<string, ShaderType>( @"..\..\shaders\cube.vs", ShaderType.VertexShader),
-                new Tuple<string, ShaderType>( @"..\..\shaders\cube.fs", ShaderType.FragmentShader),
-            });
-            lightShader = LoadProgram(new List<Tuple<string, ShaderType>> {
-                new Tuple<string, ShaderType>( @"..\..\shaders\lamp.vs", ShaderType.VertexShader),
-                new Tuple<string, ShaderType>( @"..\..\shaders\lamp.fs", ShaderType.FragmentShader),
+            program = LoadProgram(new List<Tuple<string, ShaderType>>() {
+                new Tuple<string, ShaderType>("..\\..\\shaders\\cube.vs", ShaderType.VertexShader),
+                new Tuple<string, ShaderType>("..\\..\\shaders\\cube.fs", ShaderType.FragmentShader)
             });
 
             VAO = GL.GenVertexArray();
             VBO = GL.GenBuffer();
 
-
             GL.BindVertexArray(VAO);
-            {
-                GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
-                {
-                    GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float),
-                        vertices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
-                    GL.EnableVertexAttribArray(0);
-                    GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
-                    GL.EnableVertexAttribArray(1);
-                    GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 3 * sizeof(float));
-                    GL.EnableVertexAttribArray(2);
-                    GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), 6 * sizeof(float));
-
-                }
-                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            }
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.BindVertexArray(0);
 
             cubeTexture = LoadTexture("..\\..\\textures\\container2.png");
-            cubeSpecularTexture = LoadTexture("..\\..\\textures\\container2_specular.png");
             GL.UseProgram(program);
-            GL.Uniform1(GL.GetUniformLocation(program, "material.diffuse"), 0);
-            GL.Uniform1(GL.GetUniformLocation(program, "material.specular"), 1);
-
+            GL.Uniform1(GL.GetUniformLocation(program, "texture1"), 0);
 
         }
 
@@ -271,7 +224,7 @@ namespace SpecularAndDiffuseMapping
                 GL.DeleteShader(id);
             }
 
-            
+
             return result;
         }
 
